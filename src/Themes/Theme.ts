426f1b01ddeb048e1c2121fc225281b5c37592ac
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-02-27 23:57:39 -0800

    Created my Theme object that I will use to pass my theme to my components

---
+++ src/Themes/Theme.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from "styled-components";
+
+export const Theme = {
+  palette: {
+    common: {
+      black: "#000000",
+      white: "#FFFFFF",
+    },
+    primary: {
+      main: "#4fc3f7",
+      contrastText: "#FFFFFF",
+    },
+    secondary: {
+      main: "#455a64",
+      contrastText: "#FFFFFF",
+    },
+  },
+};

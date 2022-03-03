4e7f756a9617072e51effc2bc6b0d23a4d8541a4
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-03-02 18:00:59 -0800

    Added the functionality for the Button in the GameCharacters component to navigate back to the home page when clicked

--- src/components/Game/GameCharacters.tsx
+++ src/components/Game/GameCharacters.tsx
@@ -2,11 +2,13 @@ import styled from "styled-components";
 import { ICharacter } from "../../types";
 import GameCharacter from "./GameCharacter";
 import Button from "../../utils/Button";
+import { useNavigate } from "react-router-dom";
 interface IProps {
   characters: ICharacter[];
 }
 
 const GameCharacters = ({ characters }: IProps) => {
+  const navigate = useNavigate();
   const loadGameCharacters = () => {
     return characters.map((character) => (
       <GameCharacter character={character} />
@@ -14,7 +16,10 @@ const GameCharacters = ({ characters }: IProps) => {
   };
   return (
     <GameCharactersWrapper>
-      {loadGameCharacters()} <GameButton color="black">Go Back</GameButton>
+      {loadGameCharacters()}{" "}
+      <GameButton color="black" onClick={() => navigate("/")}>
+        Go Back
+      </GameButton>
     </GameCharactersWrapper>
   );
 };
@@ -28,17 +33,21 @@ const GameCharactersWrapper = styled.section`
   gap: 2rem;
 `;
 const GameButton = styled(Button)`
-  padding: 1rem 1.5rem;
+  padding: 1rem 1.8rem;
   min-width: max-content;
   margin-left: auto;
   font-size: 1rem;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    transform: scale(1.05);
+  }
   @media only screen and (min-width: 540px) {
     font-size: 1.4rem;
     padding: 1rem 4rem;
   }
   @media only screen and (min-width: 768px) {
-    font-size: 1.8rem;
-    padding: 1.5rem 6rem;
+    font-size: 2rem;
+    padding: 1.3rem 5.3rem;
   }
 `;
 
